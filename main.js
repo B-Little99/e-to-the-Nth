5e83f3c3675c058f83bdf86dbc8224e045c70af1
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-24 14:24:56 +0000

    Added JavaScript comments

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 function factorial(j){
     let num = j;
     if (num === 0 || num === 1) {
-        return 1;
+        return 1; //The answer for factorial 0 and 1 both equal 1.
     }
     for (let i = num - 1; i >= 1; i--) {
-        num *= i;
+        num *= i; //This loop iterates from the original number taking away one each time until it reaches 1.
     } 
     return num;
 }
@@ -13,7 +13,7 @@ function e() {
     let num = document.getElementById("Data").value;
     let answer = 0;
     for (let i = 0; i <= num; i++) {
-        answer += 1 / factorial(i);
+        answer += (1 / factorial(i)); // The loop takes the factorial and applies it until the loop conditions (the user input) is met.
     }
     document.getElementById("finalResult").innerHTML = answer;
 }
