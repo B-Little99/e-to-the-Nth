80d75cdad85141e96bb8cff08d1a4163e468e5cc
Brandon Little <brandonlittle1999@hotmail.com>
2020-03-24 09:36:54 +0000

    Started HTML and JavaScript code.

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
-let n = document.getElementById("Data").value;
-
-function factorial() {
-    for (i = 1; i <=n; i++);
-    n * (n - 1);
+function factorial(){
+    let num = document.getElementById("Data").value;
+    if (num === 0 || num === 1)
+        return 1;
+    for (var i = num - 1; i >= 1; i--) {
+        num *= i;
+    }        
+    document.getElementById("finalResult").innerHTML = num;
 }
 
 function e() {
-    for (i = 1; i <= n; i++);
-    let answer = (1 / factorial(n));
-    answer += 1;
-    document.getElementById("finalResult").innerHTML = answer;
+    let num = document.getElementById("Data").value;
+    for (let i = 1; i <= num; i++) {
+        let answer = 1 / factorial(i);
+        document.getElementById("finalResult").innerHTML = answer;
+    }
 }
 
 function reset1 () {
